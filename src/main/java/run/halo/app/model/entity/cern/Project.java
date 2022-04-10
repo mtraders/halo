acb8430fcf0abb1b99849a66b21f1675c78a304b
lizc <lizc@fists.cn>
2022-04-10 17:20:06 +0800

    添加 project 表

--- src/main/java/run/halo/app/model/entity/cern/Project.java
+++ src/main/java/run/halo/app/model/entity/cern/Project.java
@@ -1,7 +1,10 @@
 package run.halo.app.model.entity.cern;
 
+import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+
+import org.apache.commons.lang3.StringUtils;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -20,5 +23,29 @@ import run.halo.app.model.entity.BasePost;
 @Entity(name = "Project")
 @DiscriminatorValue("2")
 public class Project extends BasePost {
+
+    /**
+     * 实施时间.
+     */
+    @Column(name = "peroid")
+    private String peroid;
+
+    /**
+     * 项目来源.
+     */
+    @Column(name = "project_source")
     private String projectSource;
+
+    @Override
+    public void prePersist() {
+        super.prePersist();
+
+        if (peroid == null) {
+            peroid = StringUtils.EMPTY;
+        }
+
+        if (projectSource == null) {
+            projectSource = StringUtils.EMPTY;
+        }
+    }
 }
