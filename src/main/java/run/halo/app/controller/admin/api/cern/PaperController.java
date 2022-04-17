718ee5f80962f27021432a9e7c4949fd3c63b2b8
Joe <leezhenchong@gmail.com>
2022-04-17 13:44:47 +0800

    temp commit

--- src/main/java/run/halo/app/controller/admin/api/cern/PaperController.java
+++ src/main/java/run/halo/app/controller/admin/api/cern/PaperController.java
@@ -1,10 +1,29 @@
 package run.halo.app.controller.admin.api.cern;
 
+import io.swagger.annotations.ApiOperation;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import run.halo.app.model.entity.cern.Paper;
+import run.halo.app.service.cern.PaperService;
+
+import static org.springframework.data.domain.Sort.Direction.DESC;
 
 @RestController
 @RequestMapping("/api/admin/cern/papers")
 public class PaperController {
-    public PaperController() {}
+    private final PaperService paperService;
+
+    public PaperController(PaperService paperService) {
+        this.paperService = paperService;
+    }
+
+    @GetMapping
+    @ApiOperation("Gets a page of paper")
+    public Page<Paper> pageBy(@PageableDefault(sort = "createTime", direction = DESC) Pageable pageable) {
+        return paperService.pageBy(pageable);
+    }
 }
---
+++ src/main/java/run/halo/app/model/dto/cern/paper/PaperMinimalDTO.java
@@ -0,0 +1,5 @@
+package run.halo.app.model.dto.cern.paper;
+
+public class PaperMinimalDTO {
+
+}
