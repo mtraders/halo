daffd74c9b478506ebee0bc3fd07ba8d0ed60a4c
lizc <lizc@fists.cn>
2022-04-24 15:08:35 +0800

    add post personnel entity and repository

--- src/main/java/run/halo/app/model/entity/cern/PostPersonnel.java
+++ src/main/java/run/halo/app/model/entity/cern/PostPersonnel.java
@@ -1,2 +1,68 @@
-package run.halo.app.model.entity.cern;public class PostPersonnel {
+package run.halo.app.model.entity.cern;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.Hibernate;
+import org.hibernate.annotations.GenericGenerator;
+import run.halo.app.model.entity.BaseEntity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.Table;
+import java.util.Objects;
+
+/**
+ * Post personnel.
+ *
+ * @author <a href="mailto:lizc@fists.cn">lizc</a>
+ */
+@Entity
+@Table(name = "post_personnel", indexes = {@Index(name = "post_personnel_post_id", columnList = "post_id"),
+    @Index(name = "post_personnel_personnel_id", columnList = "personnel_id")})
+@Getter
+@Setter
+@RequiredArgsConstructor
+@ToString
+public class PostPersonnel extends BaseEntity {
+    /**
+     * id.
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "custom-id")
+    @GenericGenerator(name = "custom-id", strategy = "run.halo.app.model.entity.support.CustomIdGenerator")
+    private Integer id;
+    /**
+     * Post id.
+     */
+    @Column(name = "post_id", nullable = false)
+    private Integer postId;
+
+    /**
+     * Personnel id.
+     */
+    @Column(name = "personnel_id", nullable = false)
+    private Integer personnelId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) {
+            return false;
+        }
+        PostPersonnel that = (PostPersonnel) o;
+        return id != null && Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(postId, personnelId);
+    }
 }
--- src/main/java/run/halo/app/repository/cern/PostPersonnelRepository.java
+++ src/main/java/run/halo/app/repository/cern/PostPersonnelRepository.java
@@ -1,2 +1,12 @@
-package run.halo.app.repository.cern;public interface PostPersonnelRepository {
+package run.halo.app.repository.cern;
+
+import run.halo.app.model.entity.cern.PostPersonnel;
+import run.halo.app.repository.base.BaseRepository;
+
+/**
+ * post personnel repository.
+ *
+ * @author lizc <a href=mailto:lizc@fists.cn></a>
+ */
+public interface PostPersonnelRepository extends BaseRepository<PostPersonnel, Integer> {
 }
