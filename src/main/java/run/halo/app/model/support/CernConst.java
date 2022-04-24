f1d52c2cff0945d00c1bdfcffec333ff7243b7a7
lizc <lizc@fists.cn>
2022-04-24 19:49:52 +0800

    duplicate type of news and sheet post type

--- src/main/java/run/halo/app/model/support/CernConst.java
+++ src/main/java/run/halo/app/model/support/CernConst.java
@@ -7,7 +7,7 @@ package run.halo.app.model.support;
  */
 public class CernConst {
     public static final String BASE_POST_TYPE = "0";
-    public static final String SHEET_POST_TYPE = "2";
+    public static final String SHEET_POST_TYPE = "1";
     public static final String NEWS_POST_TYPE = "2";
     public static final String NOTIFICATION_POST_TYPE = "3";
     public static final String PAPER_POST_TYPE = "4";
