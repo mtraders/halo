f945541abbad931b0bfd70d2b784409193f4c953
lizc <lizc@fists.cn>
2022-04-24 12:55:23 +0800

    add personnel admin controller method

--- src/main/java/run/halo/app/controller/admin/api/cern/PersonnelController.java
+++ src/main/java/run/halo/app/controller/admin/api/cern/PersonnelController.java
@@ -4,13 +4,26 @@ import java.util.Collections;
 import java.util.List;
 
 import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import io.swagger.annotations.ApiOperation;
 import run.halo.app.model.dto.cern.PersonnelDTO;
+import run.halo.app.model.params.cern.PersonnelParam;
 
+import javax.validation.Valid;
+
+/**
+ * Personnel controller.
+ *
+ * @author lizc mailto:lizc@fists.cn
+ */
 @RestController
 @RequestMapping("/api/admin/cern/personnel")
 public class PersonnelController {
@@ -26,5 +39,54 @@ public class PersonnelController {
     @ApiOperation(value = "List Personnel")
     public List<? extends PersonnelDTO> listPersonnel(Sort sort, Boolean more) {
         return Collections.emptyList();
+    }
+
+    /**
+     * crete personnel.
+     *
+     * @param personnelParam personnel param.
+     * @return personnel dto.
+     */
+    @PostMapping
+    @ApiOperation(value = "create personnel")
+    public PersonnelDTO createPersonnel(@Valid @RequestBody PersonnelParam personnelParam) {
+        return new PersonnelDTO();
+    }
+
+    /**
+     * get personnel detail by id.
+     *
+     * @param personnelId personnel id
+     * @return personnel detail.
+     */
+    @GetMapping("{personnelId:\\d+}")
+    @ApiOperation("Get personnel detail by id")
+    public PersonnelDTO getBy(@PathVariable("personnelId") Integer personnelId) {
+        return new PersonnelDTO();
+    }
+
+    /**
+     * update personnel by id.
+     *
+     * @param personnelId personnel id.
+     * @param personnelParam personnel param
+     * @return personnel detail.
+     */
+    @PutMapping("{personnelId:\\d+}")
+    @ApiOperation("Update a person")
+    public PersonnelDTO updateBy(@PathVariable("personnelId") Integer personnelId, @Valid @RequestBody PersonnelParam personnelParam) {
+        return new PersonnelDTO();
+    }
+
+    /**
+     * delete personnel permanently.
+     *
+     * @param personnelId personnel id.
+     * @return deleted person detail.
+     */
+    @DeleteMapping("{personnelId:\\d+}")
+    @ApiOperation("Delete person permanently.")
+    public PersonnelDTO deletePermanently(@PathVariable("personnelId") Integer personnelId) {
+        return new PersonnelDTO();
     }
 }
