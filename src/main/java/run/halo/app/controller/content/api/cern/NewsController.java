21c94b60ed077e25323f955fe7085726e3f6a980
lizc <lizc@fists.cn>
2022-04-29 21:34:40 +0800

    Enable users search published news with keywords.

--- src/main/java/run/halo/app/controller/content/api/cern/NewsController.java
+++ src/main/java/run/halo/app/controller/content/api/cern/NewsController.java
@@ -2,14 +2,24 @@ package run.halo.app.controller.content.api.cern;
 
 import io.swagger.annotations.ApiOperation;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import run.halo.app.model.entity.cern.News;
+import run.halo.app.model.enums.PostStatus;
+import run.halo.app.model.params.PostQuery;
 import run.halo.app.model.vo.cern.news.NewsDetailVO;
 import run.halo.app.model.vo.cern.news.NewsListVO;
+import run.halo.app.service.assembler.cern.NewsAssembler;
+import run.halo.app.service.cern.NewsService;
+
+import java.util.Set;
 
 /**
  * News content controller.
@@ -20,10 +30,39 @@ import run.halo.app.model.vo.cern.news.NewsListVO;
 @RequestMapping(value = "/api/content/cern/news")
 public class NewsController {
 
+    private final NewsService newsService;
+    private final NewsAssembler newsAssembler;
+
+    /**
+     * News content controller constructor.
+     *
+     * @param newsService news service.
+     * @param newsAssembler news assembler.
+     */
+    public NewsController(NewsService newsService, NewsAssembler newsAssembler) {
+        this.newsService = newsService;
+        this.newsAssembler = newsAssembler;
+    }
+
+    /**
+     * Enable users search published news with keywords.
+     *
+     * @param pageable sort by priority and create time.
+     * @param keyword search news with keyword
+     * @param categoryId search news with category id
+     * @return published news that contain keywords and specific categoryId
+     */
     @GetMapping
     @ApiOperation("Lists news")
-    public Page<NewsListVO> pageBy() {
-        return null;
+    public Page<NewsListVO> pageBy(@PageableDefault(sort = {"topPriority", "createTime"}, direction = Direction.DESC) Pageable pageable,
+                                   @RequestParam(value = "keyword", required = false) String keyword,
+                                   @RequestParam(value = "categoryId", required = false) Integer categoryId) {
+        PostQuery postQuery = new PostQuery();
+        postQuery.setKeyword(keyword);
+        postQuery.setCategoryId(categoryId);
+        postQuery.setStatuses(Set.of(PostStatus.PUBLISHED));
+        Page<News> newsPage = newsService.pageBy(postQuery, pageable);
+        return newsAssembler.convertToListVo(newsPage);
     }
 
 
